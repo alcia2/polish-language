34012cb82b6ea20c93eeea6aa945884b416da178
michalsn <michal@sniatala.pl>
2012-08-31 22:37:16 +0200

    update do wersji 3 dev

--- ftp_lang.php
+++ ftp_lang.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.2.4 or newer
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Open Software License version 3.0
+ *
+ * This source file is subject to the Open Software License (OSL 3.0) that is
+ * bundled with this package in the files license.txt / license.rst.  It is
+ * also available through the world wide web at this URL:
+ * http://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world wide web, please send an email to
+ * licensing@ellislab.com so we can send you a copy immediately.
+ *
+ * @package		CodeIgniter
+ * @author		EllisLab Dev Team
+ * @copyright	Copyright (c) 2008 - 2012, EllisLab, Inc. (http://ellislab.com/)
+ * @license		http://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @link		http://codeigniter.com
+ * @since		Version 1.0
+ * @filesource
+ */
 
 $lang['ftp_no_connection']       = "Nie można zlokalizować poprawnego ID połączenia. Upewnij się że połączenie zostało ustanowione zanim zaczniesz wykonywać jakiekolwiek operacje z plikami.";
 $lang['ftp_unable_to_connect']   = "Nie można połączyć się to Twojego serwera FTP używając podanej nazwy hosta.";
